aceaa2bfb028f0939c06ad64f8cb6c54d4ede79f
Amoralchik <dariysdemonad@gmail.com>
2025-02-20 22:28:57 +0200

    Add reqwest for simplified HTTP requests and update main function

--- omni/src/main.rs
+++ omni/src/main.rs
@@ -1,11 +1,5 @@
 use clap::Parser;
-use http_body_util::BodyExt;
-use http_body_util::Empty;
-use hyper::body::Bytes;
-use hyper::Request;
-use hyper_util::rt::TokioIo;
-use tokio::io::{self, AsyncWriteExt as _};
-use tokio::net::TcpStream;
+use reqwest;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -13,39 +7,11 @@ struct Args {
     url: String,
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Args = Args::parse();
-    let url: hyper::Uri = args.url.parse::<hyper::Uri>()?;
-
-    let host: &str = url.host().expect("uri has no host");
-    let port: u16 = url.port_u16().unwrap_or(80);
-
-    let address: String = format!("{}:{}", host, port);
-
-    let stream: TcpStream = TcpStream::connect(address).await?;
-    let io: TokioIo<TcpStream> = TokioIo::new(stream);
-    let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
-
-    tokio::task::spawn(async move {
-        if let Err(err) = conn.await {
-            println!("Connection failed: {:?}", err);
-        }
-    });
-    let authority = url.authority().unwrap().clone();
-    let req = Request::builder()
-        .uri(url)
-        .header(hyper::header::HOST, authority.as_str())
-        .body(Empty::<Bytes>::new())?;
-
-    let mut res = sender.send_request(req).await?;
-
-    while let Some(next) = res.frame().await {
-        let frame = next?;
-        if let Some(chunk) = frame.data_ref() {
-            io::stdout().write_all(chunk).await?;
-        }
-    }
-
+#[tokio::main] // Or #[async_std::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let args = Args::parse();
+    let response = reqwest::get(args.url).await?;
+    let body = response.text().await?;
+    println!("Body:\n{}", body);
     Ok(())
 }
