7a23b85d66688c6571f4d52f42430c2a56bebaf7
Amoralchik <dariysdemonad@gmail.com>
2025-02-22 23:55:32 +0200

    Add new projects

---
+++ mars/target/debug/deps/mars-d8fb53a654e14f7e.d
@@ -0,0 +1,5 @@
+/home/amoralchik/dev/f/rust-playground/mars/target/debug/deps/mars-d8fb53a654e14f7e: src/main.rs
+
+/home/amoralchik/dev/f/rust-playground/mars/target/debug/deps/mars-d8fb53a654e14f7e.d: src/main.rs
+
+src/main.rs:
---
+++ mars/target/debug/mars.d
@@ -0,0 +1,1 @@
+/home/amoralchik/dev/f/rust-playground/mars/target/debug/mars: /home/amoralchik/dev/f/rust-playground/mars/src/main.rs
