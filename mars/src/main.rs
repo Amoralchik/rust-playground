7cea266ba1f408aed9d22bd652aa81ee6604d6e0
Amoralchik <dariysdemonad@gmail.com>
2025-02-25 23:31:21 +0200

    Enhance weight calculation program to accept user input for weight in kilograms

--- mars/src/main.rs
+++ mars/src/main.rs
@@ -1,8 +1,14 @@
+use std::io::{self, Write};
+
 fn main() {
-    let mars_weight: f32 = calculate_weight_on_mars(100.0);
+    print!("Enter your weight in kilograms: ");
+    io::stdout().flush().unwrap(); // displayed print before input
+    let mut input: String = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let weight: f32 = input.trim().parse().unwrap();
+    let mars_weight: f32 = calculate_weight_on_mars(weight);
     println!("Weight on mars: {}", mars_weight);
 }
-
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
     (weight / 9.81) * 3.711
